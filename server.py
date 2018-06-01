28123b8e2c23e695717088e5d48640f1e75a17f2
Marta <marta.rose@gmail.com>
2018-05-31 20:49:42 -0700

    cleaning up code and adding comments

--- server.py
+++ server.py
@@ -20,17 +20,17 @@ def validate():
         'email': email_address #request.form['email']
     }
     user_email = mysql.query_db(query, data)
-    #section above queries for any emails in the DB that match the email recently entered in the form on index.html. If statement then decides what to do next based on a few conditions..
+    #section above queries for any emails in the DB that match the email recently entered in the form on index.html. If statement below then decides what to do next based on a few conditions..
 
-    if len(user_email) == 0:
+    if len(user_email) == 0: #if there is no email in DB that matches email entered in form:
         query = "INSERT INTO users (email, created_at, updated_at) VALUES (:email, NOW(), NOW())"
         data = {
             'email': email_address #request.form['email']
         }
-        mysql.query_db(query, data)
+        mysql.query_db(query, data) #inserts new email from form in the DB
         flash("The Email you entered {}".format(request.form['email']) + " " + "is a valid email!", 'success')
         return redirect('/success')
-    else:
+    else: #entered email did match an email in the DB so len of user_email was not 0
         flash("Email is not valid!",'error')
         return redirect('/')
 
